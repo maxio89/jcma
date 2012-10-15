90feb060e7ba2bc7c8539bc90727f97f684e4a48
Piotr Kozlowski <maxio89@gmail.com>
2012-10-15 12:44:45 +0200

    little changes

--- src/main/java/jcma/LoginView.java
+++ src/main/java/jcma/LoginView.java
@@ -1,5 +1,7 @@
 package jcma;
 
+import javax.faces.context.FacesContext;
+
 /**
  * Created with IntelliJ IDEA.
  * User: piotrek
@@ -8,4 +10,60 @@ package jcma;
  * To change this template use File | Settings | File Templates.
  */
 public class LoginView {
+
+    private String password;
+    private String userName;
+    private String buttonText = "login";
+    private int cnt = 0;
+    private boolean session = false;
+
+
+    // --------------------- GETTER / SETTER METHODS ---------------------
+//    public String getResponse() {
+//        return response;
+//    }
+//
+//    public void setResponse(String response) {
+//        this.response = response;
+//    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String username) {
+        this.userName = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+    // -------------------------- OTHER METHODS --------------------------
+    public void checkMe() {
+
+
+        if (!session) {
+
+            if (userName.equals("user") && password.equals("hasl0")) {
+                buttonText = "logout";
+                cnt++;
+                //response = "Welcome " + username + "!";
+                session = true;
+            } else {
+                //response = "Wrong username or password!!!";
+            }
+
+        } else {
+            FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
+            //response = "Logout!";
+            session = false;
+        }
+
+    }
 }
